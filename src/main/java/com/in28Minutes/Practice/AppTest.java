eb93d79bf310ca87965205dcac5bcaa41f354cfd
SrikanthRam <SrikanthRam@10.0.0.215>
2020-01-06 06:22:30 -0500

    My First commit to GitHub.More on the way

---
+++ src/main/java/com/in28Minutes/Practice/AppTest.java
@@ -0,0 +1,14 @@
+package com.in28Minutes.Practice;
+
+public class AppTest {
+
+	public AppTest() {
+		System.out.println("Constructor Invoked");
+	}
+
+	public static void main(String[] args) {
+		AppTest apptest = new AppTest();
+		System.out.println("Main Method called");
+	}
+
+}
---
+++ src/test/java/AppMainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import com.in28Minutes.Practice.AppTest;
+
+import junit.framework.TestCase;
+
+public class AppMainTest extends TestCase {
+
+	
+	public void testAppTestConstructor() {
+		AppTest test = new AppTest();
+		assertNotNull(test);
+	}
+}
